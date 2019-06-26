f8a2825ef2c2c8664598257cb81153547dfee14d
siddharthamaji <siddhartha.maji@switch2web.com>
2019-06-26 19:08:24 +0530

    UI modified

--- src/sample/AppUtil.java
+++ src/sample/AppUtil.java
@@ -2,12 +2,18 @@ package sample;
 
 import javax.tools.*;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AppUtil {
 
+    void generateObj() throws Exception {
 
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
@@ -19,5 +21,10 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        try {
+            new AppUtil().generateObj();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
