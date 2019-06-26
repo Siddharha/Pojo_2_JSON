1a3c239da7401cbbb94c55c99134418c8b213c72
siddharthamaji <siddhartha.maji@switch2web.com>
2019-06-26 16:58:11 +0530

    UI modified

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 import javax.tools.JavaCompiler;
 
 public class Controller {
 
+    @FXML
+    TextArea etPojo;
 }
